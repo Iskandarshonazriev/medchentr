e61e20493f2a6eb06ad40e7c62e522e9eb48b198
iskandarshonazriev <iskandarshonazriev@gmail.com>
2025-05-26 18:35:40 +0500

    med

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens:{
+      "tel":{max:'510px'},
+      //@media (max-width:510px){}
+    }
+  },
+  plugins: [],
+}
+
